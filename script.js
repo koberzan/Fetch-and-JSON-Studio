a108a17d2db14c408270d095a7e6b881816f1f39
Kaylee <kayleeoberzan@gmail.com>
2019-12-11 20:36:19 -0600

    clear tree

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener('load', function(){
+
+    let container = document.getElementById('container');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then( response => response.json())
+    .then( function(response) {
+        
+    response.forEach(astronaut => {
+
+    let temp = `<div class="astronaut">
+                <div class="bio">
+                <h3>${response.firstName}</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                </div>
+                <img class="avatar">
+            </div>`
+    
+
+    });    
+   
+    }); 
+
+
+
+});
